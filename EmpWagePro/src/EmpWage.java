c1ec1a99b842f1f1290f1b179570209849c3adaf
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-17 21:07:36 +0530

    Added : part time wage of employee is added

--- EmpWagePro/src/EmpWage.java
+++ EmpWagePro/src/EmpWage.java
@@ -3,6 +3,8 @@ public class EmpWage {
 		static int workTime=8;
 		static String Employee;
 		static int dailyEmpWage;
+		static int partTimeEmpWage;
+		static int partTime=4;
 	    public static void Attendance() {
 		int emp=(int)(Math.random()*2);
 		if(emp==1)
@@ -27,12 +29,27 @@ public class EmpWage {
 			dailyEmpWage=0;
 			System.out.println("Daily wage of Employee is :"+dailyEmpWage);
 		}
-		    	
 	    }
+	    
+	    public static void partTimeWage()
+	    {
+	    	if(Employee=="present")
+	    	 {
+	    	partTimeEmpWage=(empWagePerHour*partTime);
+	    	System.out.println("part time of Employee is :"+partTimeEmpWage);
+		    }
+		else
+		{
+			partTimeEmpWage=0;
+			System.out.println("part time of employee wage of Employee is :"+partTimeEmpWage);
+		}
+	    }
+	    
 	    
 		public static void main(String args[])
 		{
 			Attendance();
 			dailyWage();
+			partTimeWage();
 		}
 	}
