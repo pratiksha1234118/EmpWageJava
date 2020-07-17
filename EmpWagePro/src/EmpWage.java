7c83686ef77ce22a8026b85ead98dcb6ccf8d5d9
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-17 20:57:00 +0530

    Added :daily wage of employee is added

--- EmpWagePro/src/EmpWage.java
+++ EmpWagePro/src/EmpWage.java
@@ -1,17 +1,38 @@
 public class EmpWage {
+		static int empWagePerHour=20;
+		static int workTime=8;
+		static String Employee;
+		static int dailyEmpWage;
 	    public static void Attendance() {
 		int emp=(int)(Math.random()*2);
 		if(emp==1)
-		{
+		{ 
+			Employee="present";
 			System.out.println("employee is present");
 		}
 		else
-		{
+		{	
 			System.out.println("employee is absent");
 		}
 	    }
+	    public static void dailyWage()
+	    {
+	    	if(Employee=="present")
+	    	 {
+	    	dailyEmpWage=(empWagePerHour*workTime);
+	    	System.out.println("Daily Wage of Employee is :"+dailyEmpWage);
+		    }
+		else
+		{
+			dailyEmpWage=0;
+			System.out.println("Daily wage of Employee is :"+dailyEmpWage);
+		}
+		    	
+	    }
+	    
 		public static void main(String args[])
 		{
 			Attendance();
+			dailyWage();
 		}
 	}
